088de546e6dc7045f264764e122fdfdd61b483a4
Akbar Hamaminatu <akbar@ngoding.id>
2024-05-25 20:32:02 +0700

    feat: change site metadata

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const siteConfig: SiteConfig = {
   subtitle: 'Developer Blog',
   lang: 'id',
   themeColor: {
-    hue: 150, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 300, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: true, // Hide the theme color picker for visitors
   },
   banner: {
@@ -44,7 +44,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpeg',
-  name: 'Moshi 🍥',
+  name: 'Moshi',
   bio: '/* #AndroidDev fueled by curiosity. Debugging code & life 🌱🐞 Sharing my journey & lessons learned 🍵 */',
   links: [
     {
