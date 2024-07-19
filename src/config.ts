50c828a5ec63f7073660bc448d6f1eb5afebc4b1
Akbar Hamaminatu <akbar@ngoding.id>
2024-07-19 10:09:19 +0700

    fix: fix invalid image path

--- src/config.ts
+++ src/config.ts
@@ -3,21 +3,21 @@ import type {
   NavBarConfig,
   ProfileConfig,
   SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+} from "./types/config";
+import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: 'Moshi',
-  subtitle: 'Developer Blog',
-  lang: 'id',
+  title: "Moshi",
+  subtitle: "Developer Blog",
+  lang: "id",
   themeColor: {
     hue: 300, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: true, // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center', // Equivalent to object-position, defaults center
+    src: "assets/images/banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: "center", // Equivalent to object-position, defaults center
   },
   favicon: [
     // Leave this array empty to use the default favicon
@@ -27,38 +27,38 @@ export const siteConfig: SiteConfig = {
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
   ],
-}
+};
 
 export const navBarConfig: NavBarConfig = {
   links: [LinkPreset.Home, LinkPreset.Archive, LinkPreset.About],
-}
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/avatar.jpeg',
-  name: 'Moshi',
-  bio: '/* #AndroidDev fueled by curiosity. Debugging code & life 🌱🐞 Sharing my journey & lessons learned 🍵 */',
+  avatar: "assets/images/avatar.jpeg",
+  name: "Moshi",
+  bio: "/* #AndroidDev fueled by curiosity. Debugging code & life 🌱🐞 Sharing my journey & lessons learned 🍵 */",
   links: [
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',
-      url: 'https://twitter.com/gradlew_',
+      name: "Twitter",
+      icon: "fa6-brands:twitter",
+      url: "https://twitter.com/gradlew_",
     },
     {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/akbarhmu',
+      name: "GitHub",
+      icon: "fa6-brands:github",
+      url: "https://github.com/akbarhmu",
     },
     {
-      name: 'Play Store',
-      icon: 'fa6-brands:google-play',
-      url: 'https://play.google.com/store/apps/dev?id=9002881621187826045',
+      name: "Play Store",
+      icon: "fa6-brands:google-play",
+      url: "https://play.google.com/store/apps/dev?id=9002881621187826045",
     },
   ],
-}
+};
 
 export const licenseConfig: LicenseConfig = {
   enable: true,
-  name: 'CC BY-NC-SA 4.0',
-  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-  author: 'Akbar Hamaminatu',
-}
+  name: "CC BY-NC-SA 4.0",
+  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+  author: "Akbar Hamaminatu",
+};
